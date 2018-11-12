baa591658ace6fd22c743aea627eeabd926d1e76
Robert Eriksson <roberteriksson12@gmail.com>
2018-11-12 17:46:09 +0100

    Bajsmacka
    Gitskit

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace Calculator
+{
+    public class Calculator
+    {
+        private int firstNumber;
+        public int FirstNumber {
+            get { return firstNumber; }
+            set { firstNumber = Math.Abs(value); }
+        }
+        private int secondNumber;
+        public int SecondNumber
+        {
+            get { return secondNumber; }
+            set { secondNumber = Math.Abs(value); }
+        }
+        private int numberInMemory;
+        public int NumberInMemory
+        {
+            get { return numberInMemory; }
+            set { numberInMemory = Math.Abs(value); }
+        }
+
+        public Calculator()
+        {
+            FirstNumber = 0;
+            SecondNumber = 0;
+            NumberInMemory = 0;
+        }
+
+        public int SumNumbers()
+        {
+            return FirstNumber + SecondNumber;
+        }
+
+        public int MultiplyNumbers()
+        {
+            return FirstNumber * SecondNumber;
+        }
+
+        public int SumInBetween()
+        {
+            int Sum = 0;
+            for (int i = FirstNumber; i <= SecondNumber; i++)
+            {
+                Sum += i;
+            }
+
+            return Sum;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("First number: {0}\n" +
+                                 "Second number: {1}\n" +
+                                 "Number in memory: {2}",
+                                 FirstNumber,
+                                 SecondNumber,
+                                 NumberInMemory);
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool run = true;
+            Calculator calc = new Calculator();
+
+            while (run)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Add two numbers\n" +
+                                  "2. Multiply two numbers\n" +
+                                  "3. Sum the numbers between two numbers\n" +
+                                  "4. Add number to memory\n" +
+                                  "5. Clear memory\n" +
+                                  "6. Print calculator contents\n" +
+                                  "7. Quit");
+                Console.Write("Please type a number from the menu: ");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        GetFirstAndSecondNumber();
+                        Add();
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        GetFirstAndSecondNumber();
+                        Multiply();
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        GetFirstAndSecondNumber();
+                        Sum();
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        AddNumberToMemory();
+                        Console.ReadLine();
+                        break;
+                    case 5:
+                        ClearMemory();
+                        Console.ReadLine();
+                        break;
+                    case 6:
+                        Console.WriteLine(calc.ToString());
+                        Console.ReadLine();
+                        break;
+                    case 7:
+                        Console.WriteLine("Goodbye!");
+                        run = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+
+            void GetFirstAndSecondNumber()
+            {
+                Console.Write("Please type the first number: ");
+                calc.FirstNumber = int.Parse(Console.ReadLine());
+                Console.Write("Please type the second number: ");
+                calc.SecondNumber = int.Parse(Console.ReadLine());
+            }
+
+            void GetNumber()
+            {
+                Console.Write("Please type the number: ");
+                calc.NumberInMemory = int.Parse(Console.ReadLine());
+            }
+
+            void AddNumberToMemory()
+            {
+                GetNumber();
+                Console.WriteLine("Number successfully added to memory.");
+            }
+
+            void ClearMemory()
+            {
+                calc.NumberInMemory = 0;
+                Console.WriteLine("Number in memory cleared.");
+            }
+
+            void Add()
+            {
+                Console.WriteLine("Sum of {0} + {1} = {2}", 
+                                  calc.FirstNumber, 
+                                  calc.SecondNumber, 
+                                  calc.SumNumbers());
+            }
+
+            void Multiply()
+            {
+                Console.WriteLine("Sum of {0} * {1} = {2}", 
+                                  calc.FirstNumber, 
+                                  calc.SecondNumber, 
+                                  calc.MultiplyNumbers());
+            }
+
+            void Sum()
+            {
+                Console.WriteLine("Sum of {0} to {1} = {2}",
+                                  calc.FirstNumber,
+                                  calc.SecondNumber,
+                                  calc.SumInBetween());
+            }
+        }
+    }
+}
